5c6dde34c6bb13547482f46b9740b6d04f1eea90
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2011-09-19 10:48:58 -0300

    fetchrecords adaptada ao que pede o enunciado

--- record.c
+++ record.c
@@ -24,92 +24,23 @@ int stoprecording(){
   return -1;
 }
 
-/* Tranforma o numero da System Call no respectivo nome */
-char* translatesyscall(int sc){
-  switch(sc){
-    case 1:
-      return "SYS_fork";
-    case 2:
-      return "SYS_exit";
-    case 3:
-      return "SYS_wait";
-    case 4:
-      return "SYS_pipe";
-    case 5:
-      return "SYS_write";
-    case 6:
-      return "SYS_read";
-    case 7:
-      return "SYS_close";
-    case 8:
-      return "SYS_kill";
-    case 9:
-      return "SYS_exec";
-    case 10:
-      return "SYS_open";
-    case 11:
-      return "SYS_mknod";
-    case 12:
-      return "SYS_unlink";
-    case 13:
-      return "SYS_fstat";
-    case 14:
-      return "SYS_link";
-    case 15:
-      return "SYS_mkdir";
-    case 16:
-      return "SYS_chdir";
-    case 17:
-      return "SYS_dup";
-    case 18:
-      return "SYS_getpid";
-    case 19:
-      return "SYS_sbrk";
-    case 20:
-      return "SYS_sleep";
-    case 21:
-      return "SYS_uptime";
-    case 22:
-      return "SYS_startrecording";
-    case 23:
-      return "SYS_stoprecording";
-    case 24:
-      return "SYS_fetchrecords";
-    default:
-      return "Doesn't exist";
-  }
-}
-
-void printrecord(struct record rec){  
-  switch(rec.type){
-    case SYSCALL_NO:
-        cprintf("SYSCALL_NO: %s\n",translatesyscall(rec.value.intval));
-        break;
-    case ARG_INTEGER:
-        cprintf("ARG_INTEGER: %d\n",rec.value.intval);
-        break;
-    case ARG_POINTER:
-        cprintf("ARG_POINTER: %p\n",rec.value.ptrval);
-        break;
-    case ARG_STRING:
-        cprintf("ARG_STRING: %s\n",rec.value.strval);
-        break;
-    case RET_VALUE:
-        cprintf("RET_VALUE: %d\n",rec.value.intval);
-        break;
-    }
-}
-
 int fetchrecords(struct record *records, int num_records){
-  int i;
+  reclist runner;
+  int cnt;  
 
-  if( records == 0)
-    return fetchrecordslist(proc->recl);
-  else{
-    for( i = 0; (i < num_records); i++)
-      printrecord(records[i]);
-    return i;
+  if( num_records == 0){
+    for(cnt = 0, runner = proc->recl; runner != 0; runner = runner->next, cnt++);
+  }else{
+    cnt = 0;
+    runner = proc->recl;
+    
+    while(cnt < num_records && runner != 0){
+      records[cnt++] = runner->rec;
+      runner = runner->next;
+    }
   }
+  
+  return cnt;
 }
 
 int addrecordtolist(reclist *list, struct record rec){
@@ -136,51 +67,8 @@ int addrecordtolist(reclist *list, struct record rec){
   return 1;
 }
 
-reclist copyrecordslist(reclist list){
-  reclist new, aux, ant;
-  new = aux = ant = 0;
-
-  cprintf("copyrecordslist\n");
-  
-  /* Como a lista não possui cabeça tratamos o primeiro caso separadamente */
-  if(list != 0){
-      if( (aux = (reclist) kalloc()) == 0 )
-        return 0;
-      aux -> rec = list -> rec;
-      aux -> next = 0;
-      ant = aux;
-      list = list -> next;
-      new = aux;
-      aux = 0;
-  } 
-  while( list != 0 ){
-      if( (aux = (reclist) kalloc()) == 0 )
-        return 0;
-      aux -> rec = list -> rec;
-      ant -> next = aux;
-      ant = aux;
-      aux = 0;
-      list = list -> next;
-  }
-  return new;
-}
-
-int fetchrecordslist(reclist list){
-  reclist runner;
-  int cnt;  
-
-  cprintf("fetchrecordslist\n");
-
-  for(cnt = 0, runner = list; runner != 0; runner = runner->next, cnt++)
-    printrecord(runner->rec);
-    
-  return cnt;
-}
-
 int releaserecordslist(reclist list){
   reclist runner, next;
-
-  cprintf("releaserecords\n");
   
   runner = list; 
   
